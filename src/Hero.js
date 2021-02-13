8099982563cf33d7cb236aafa6fa6a6df6697b36
manthanjgodhasara <69115559+manthanjgodhasara@users.noreply.github.com>
2021-02-13 12:48:44 +0530

    Add files via upload

---
+++ src/Hero.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import phoneImg from './images/phone.svg'
+import {useGlobalContext} from './context'
+
+const Hero = () => {
+  const {closeSubmenu} = useGlobalContext();
+
+  return (
+    <section className="hero" onMouseOver={closeSubmenu}>
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments Infrastructure for the internet</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+            assumenda nostrum culpa velit deleniti. Nemo magnam libero quae enim
+            reiciendis?
+          </p>
+          <button className="btn">Start Now</button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  );
+}
+
+export default Hero
